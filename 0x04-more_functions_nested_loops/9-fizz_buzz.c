382d012b1d30bf5bb1fee3d766d7a6b854bd5463
E-buez <ehujuochibueze@gmail.com>
2022-10-20 04:36:39 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the numbers from 1 to 100
+ * for fizz and buzz
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i == 100)
+			continue;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
