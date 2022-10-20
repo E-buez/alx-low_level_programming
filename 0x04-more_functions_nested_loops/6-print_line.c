c74c829c17330481b164613eac30469ddfa2ca06
E-buez <ehujuochibueze@gmail.com>
2022-10-20 04:22:28 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Print lines
+ * @n: how to many lines to print
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
